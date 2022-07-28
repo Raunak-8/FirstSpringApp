0893847a5ded723ecdc20e0cda59ae41ed07c859
Raunak-8 <raunakgupta61@gmail.com>
2022-07-28 11:32:13 +0530

    Upto Step 33 Security n setting up of new todo user

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -2,6 +2,9 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -26,16 +29,18 @@ public class TodoController {
 	
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
-		List<Todo> todos = todoService.findByUsername("in28minutes");
+		String username = getLoggedinUsername(model);
+		List<Todo> todos = todoService.findByUsername(username);
 		model.addAttribute("todos", todos);
 		return "listTodos";
 	}
-	
+
+
 	
 	//GET,POST
 	@RequestMapping(value="add-todo", method=RequestMethod.GET)
 	public String showNewTodoPage(ModelMap model) {
-		String username = (String)model.get("name");
+		String username = getLoggedinUsername(model);
 		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
 		model.put("todo", todo);
 		return "todo";
@@ -49,7 +54,7 @@ public class TodoController {
 		{
 			return "todo";
 		}
-		String username = (String)model.get("name");
+		String username = getLoggedinUsername(model);
 		todoService.addTodo(username, todo.getDescription(), 
 				todo.getTargetDate(), false);
 		return "redirect:list-todos";
@@ -77,10 +82,16 @@ public class TodoController {
 			return "todo";
 		}
 		
-		String username = (String)model.get("name");
+		String username = getLoggedinUsername(model);
 		todo.setUsername(username);
 		todoService.updateTodo(todo);
 		return "redirect:list-todos";
+	}
+	
+	private String getLoggedinUsername(ModelMap model) {
+		Authentication authentication = 
+				SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 
 
--- src/main/resources/META-INF/resources/WEB-INF/JSP/listTodos.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/JSP/listTodos.jsp
@@ -1,4 +1,4 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ include file="common/header.jspf" %>
 
 <html>
 	<head>
@@ -6,6 +6,8 @@
 		<title>List Todo Page</title>
 	</head>
 <body>
+	<%@ include file="common/navigation.jspf" %>
+	
 	<div class="container">
 		<h1>Your Todos</h1>
 		<table class="table">
@@ -33,7 +35,4 @@
 		</table>
 		<a href="add-todo" class="btn btn-success">Add Todo</a>
 	</div>
-		<script src="webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
-		<script src="webjars/jquery/3.6.0/jquery.min.js"></script>
-</body>
-</html>
+		<%@ include file="common/footer.jspf" %>
